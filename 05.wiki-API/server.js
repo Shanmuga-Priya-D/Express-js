af896347c19fc24fdb408a15f2cb88ad433d1d43
Shanmuga Priya D <66220154+Shanmuga-Priya-D@users.noreply.github.com>
2020-06-23 22:46:37 +0530

    RESTful API

---
+++ 05.wiki-API/server.js
@@ -0,0 +1,109 @@
+const express=require("express")
+const bodyParser=require("express")
+const mongoose=require("mongoose")
+const ejs=require("ejs")
+const app=express()
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useUnifiedTopology: true,useNewUrlParser:true});
+
+const articleSchema={
+    title:String,
+    content:String
+}
+
+const Article=mongoose.model("Article",articleSchema);
+
+/////////////////////////////////// Request targetting all articles ////////////////////////////////////////////////
+
+app.route("/articles")
+.get(function(req,res){
+    Article.find(function(err,foundArticles){
+        if(!err){
+        res.send(foundArticles)
+        }
+        else{
+            res.send(err)
+        }
+    })
+})
+.post(function(req,res){
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+
+    const newArticle=new Article({
+       title:req.body.title,
+       content:req.body.content
+    })
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Successfully send articles")
+        }
+        else{
+            res.send(err)
+        }
+    })
+})
+.delete(function(req,res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("successfully deleted all articles");
+        }
+        else{
+            res.send(err);
+        }
+    })
+});
+/////////////////////////////////// Request targetting a specific article ////////////////////////////////////////////////
+
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+    Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+        if(foundArticle){
+            res.send(foundArticle)
+        }
+        else{
+            res.send("No articles matching the title was found");
+        }
+    })
+})
+.put(function(req,res){
+    Article.update(
+        {title:req.params.articleTitle},
+        {title:req.body.title,content:req.body.content},
+        {overwrite:true},
+        function(err){
+            if(!err){
+                res.send("successfully updated articles");
+            }
+        }
+    )
+})
+.patch(function(req,res){
+    Article.update(
+        {title:req.params.articleTitle},
+        {$set:req.body},
+        function(err){
+            if(!err){
+                res.send("successfully updated article")
+            }
+        
+    })
+})
+.delete(function(req,res){
+    Article.deleteOne(
+        {title:req.params.articleTitle},
+        function(err){
+            if(!err){
+                res.send("successfully deleted the article")
+            }
+        }
+    )
+})
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+})
